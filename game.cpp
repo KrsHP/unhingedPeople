ceeaf0a9cfdb0ef93ca53096afc0abf7e6c5ddc5
adisn2005 <adisuryan2005@gmail.com>
2026-05-20 23:42:07 +0700

    Menambahkan fitur item

--- game.cpp
+++ game.cpp
@@ -5,6 +5,133 @@
 
 using namespace std;
 
+// ========================================
+// linked list inventorynya
+// ========================================
+
+struct ItemNode
+{
+    string itemName;
+    ItemNode *next;
+};
+
+struct Inventory
+{
+    ItemNode *head;
+
+    void init()
+    {
+        head = NULL;
+    }
+
+    void addItem(string item)
+    {
+        ItemNode *newItem = new ItemNode;
+
+        newItem->itemName = item;
+        newItem->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newItem;
+        }
+        else
+        {
+            ItemNode *temp = head;
+
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = newItem;
+        }
+    }
+
+    void showInventory()
+    {
+        ItemNode *temp = head;
+
+        cout << "ITEM : ";
+
+        if (temp == NULL)
+        {
+            cout << "KOSONG";
+        }
+
+        int nomor = 1;
+
+        while (temp != NULL)
+        {
+            cout << "[" << nomor << "] " << temp->itemName;
+
+            if (temp->next != NULL)
+            {
+                cout << " | ";
+            }
+
+            temp = temp->next;
+            nomor++;
+        }
+
+        cout << endl;
+    }
+
+    int countItem()
+    {
+        int total = 0;
+
+        ItemNode *temp = head;
+
+        while (temp != NULL)
+        {
+            total++;
+            temp = temp->next;
+        }
+
+        return total;
+    }
+
+    bool useSelectedItem(int pilihan, string &item)
+    {
+        if (head == NULL)
+        {
+            return false;
+        }
+
+        ItemNode *temp = head;
+        ItemNode *prev = NULL;
+
+        int nomor = 1;
+
+        while (temp != NULL)
+        {
+            if (nomor == pilihan)
+            {
+                item = temp->itemName;
+
+                if (prev == NULL)
+                {
+                    head = temp->next;
+                }
+                else
+                {
+                    prev->next = temp->next;
+                }
+
+                delete temp;
+                return true;
+            }
+
+            prev = temp;
+            temp = temp->next;
+            nomor++;
+        }
+
+        return false;
+    }
+};
+
 //Fungsi Ngubah Warna UI Battle
 void setColor(int warna){
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), warna);
